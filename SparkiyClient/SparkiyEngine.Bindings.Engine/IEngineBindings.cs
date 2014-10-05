84fc89122e58ee11a9d7c291d1064415fe7e2a98
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-10-05 19:57:12 +0200

    Added LuaImplementation and Bindings projects

---
+++ SparkiyClient/SparkiyEngine.Bindings.Engine/IEngineBindings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparkiyEngine.Bindings.Engine
+{
+    public interface IEngineBindings
+    {
+    }
+}
