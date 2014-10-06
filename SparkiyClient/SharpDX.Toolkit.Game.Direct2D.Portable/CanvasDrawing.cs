cef14fe11c2ba9f0e1152fd7652712aee8f09d7f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-10-06 19:29:44 +0200

    Removed Direct repo

--- SparkiyClient/SharpDX.Toolkit.Game.Direct2D.Portable/CanvasDrawing.cs
+++
@@ -1,4 +0,0 @@
-namespace SharpDX.Toolkit.Direct2D.Test.CanvasStub {
-    public abstract class CanvasDrawing : CanvasObject {
-    }
-}
--- SparkiyClient/SharpDX.Toolkit.Game.Direct2D.Portable/CanvasState.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SharpDX.Toolkit.Direct2D.Test.CanvasStub {
-    public enum CanvasState {
-        Cache,
-        Refresh,
-        Append
-    }
-}
