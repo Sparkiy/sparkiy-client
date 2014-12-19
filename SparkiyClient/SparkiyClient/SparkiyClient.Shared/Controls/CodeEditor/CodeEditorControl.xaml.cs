2d546fbbf8839e9858f4066d901a6b24944b2efb
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-12-19 18:11:53 +0100

    Updated old code Editor code to support code changes

--- SparkiyClient/SparkiyClient/SparkiyClient.Shared/Controls/CodeEditor/CodeEditorControl.xaml.cs
+++ SparkiyClient/SparkiyClient/SparkiyClient.Shared/Controls/CodeEditor/CodeEditorControl.xaml.cs
@@ -1,35 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System;
-using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 using SparkiyClient.Common.Controls;
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace SparkiyClient.Controls
+namespace SparkiyClient.Controls.CodeEditor
 {
 	public sealed partial class CodeEditorControl : UserControl, ICodeEditor
     {
 	    public event EventHandler OnCodeChanged;
 
-	    private string debugTemplate = "function Created()\n\t\nend\n\nfunction Started()\n\t\nend\n\nfunction Draw()\n\t\nend\n\nfunction Touched(state, x, y)\n\t\nend\n\nfunction Stopped()\n\t\nend\n";
-
-	    private string fontFamily = "Consolas";
-	    private double fontSize = 16;
-
-	    private string tabValue = "    ";
+	    private const string DebugTemplate = "function Created()\n\t\nend\n\nfunction Started()\n\t\nend\n\nfunction Draw()\n\t\nend\n\nfunction Touched(state, x, y)\n\t\nend\n\nfunction Stopped()\n\t\nend\n";
 
 
         public CodeEditorControl()
@@ -41,42 +22,20 @@ namespace SparkiyClient.Controls
 
 	    private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
 	    {
-            // Setup editor style
-            //this.CodeEditor.FontFamily = new FontFamily(fontFamily);
-	        //this.CodeEditor.FontSize = this.fontSize;
-
 #if DEBUG
-		    this.CodeEditor.Text = this.debugTemplate;
+		    this.CodeEditor.Text = DebugTemplate;
 #endif
 
-		    //      this.CodeEditor.TextChanged += (o, args) =>
-		    //{
-		    //    this.OnCodeChanged?.Invoke(this, null);
-		    //};
-		    //      this.CodeEditor.KeyDown += RichEditBoxOnKeyDown;
+			this.CodeEditor.TextView.TextChanged += (o, args) =>
+		    {
+		        this.OnCodeChanged?.Invoke(this, null);
+		    };
 	    }
 
-	    private void RichEditBoxOnKeyDown(object sender, KeyRoutedEventArgs e)
-	    {
-	        //if (e.Key == VirtualKey.Tab)
-	        //{
-	        //    this.RichEditBox.Document.Selection.TypeText(this.tabValue);
-	        //    e.Handled = true;
-	        //}
-	    }
-
-
-		//public string Code
-		//{
-		//	get
-		//	{
-		//		string code = String.Empty;
-		//		this.RichEditBox.Document.GetText(TextGetOptions.None, out code);
-		//		return code;
-		//	}
-		//	set { this.RichEditBox.Document.SetText(TextSetOptions.None, value ?? String.Empty); }
-		//}
-
-		public string Code { get; set; }
+		public string Code
+		{
+			get { return this.CodeEditor.Text; }
+			set { this.CodeEditor.Text = value; }
+		}
     }
 }
